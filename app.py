4db202f2f0cd8ae30ea4938eee4f1cfb8ef6f3da
Ben Matheja <post@benmatheja.de>
2017-09-09 15:47:58 +0200

    added debug out for requests

--- app.py
+++ app.py
@@ -15,10 +15,26 @@ import datetime
 app = Flask(__name__)
 
 
+@app.before_first_request
+def setup_logging():
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler = RotatingFileHandler('todoist-flask.log', maxBytes=2000000, backupCount=1)
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+
+
+@app.before_request
+def log_request_info():
+    app.logger.debug('Headers: %s', request.headers)
+    app.logger.debug('Body: %s', request.get_data())
+
+
 @app.route('/todoist')
 def index():
     return jsonify({'status': 'accepted',
                     'health': 'ok'}), 200
+
 
 def create_task(delivery_id):
     now = datetime.datetime.now()
@@ -32,8 +48,9 @@ def create_task(delivery_id):
     # app.logger.debug('Clock out at ' + clockout_time)
 
     # If devmode is False Push Event to Todoist and create new Tasks for Clocking Out
-    app.logger.info('Create Todoist Task: ' + 'Gehen (Gekommen: ' + clockin_time + ') due at ' + clockout_time + ' for ' + delivery_id)
-    #if not settings.DEV_MODE:
+    app.logger.info(
+        'Create Todoist Task: ' + 'Gehen (Gekommen: ' + clockin_time + ') due at ' + clockout_time + ' for ' + delivery_id)
+    # if not settings.DEV_MODE:
     # app.logger.debug('Preparing Request for Todoist')
     api = todoist.TodoistAPI(token=settings.TODOIST_API_ACCESS)
     # app.logger.debug('API connected')
@@ -52,13 +69,14 @@ def handle_event():
     event_id = request.headers.get('X-Todoist-Delivery-ID')
     # Check if user-agent matches to todoist webhooks
     if request.headers.get('USER-AGENT') == 'Todoist-Webhooks':
-        app.logger.debug('Incoming Request from ' + request.headers.get('X-Real-IP') + ' with matching user-agent' + event_id)
+        app.logger.debug(
+            'Incoming Request from ' + request.headers.get('X-Real-IP') + ' with matching user-agent' + event_id)
         # Maybe use a statemachine here? Need to hop to else case from this one
         if not request.json:
             app.logger.info('Incoming Request is not valid JSON ' + event_id)
             end = datetime.datetime.now()
             app.logger.info('Processed request from ' + request.headers.get('X-Real-IP') + ' in ' + (
-                end - begin_time).seconds.__str__() + 's status:rejected ' + event_id )
+                end - begin_time).seconds.__str__() + 's status:rejected ' + event_id)
             return jsonify({'status': 'rejected',
                             'reason': 'malformed request'}), 400
 
@@ -73,14 +91,14 @@ def handle_event():
         app.logger.debug('Processed Request Payload ' + event_id)
         app.logger.debug('start comparing ' + calculated_hmac + ' with ' + request_hmac + ' for ' + event_id)
         if request_hmac == calculated_hmac:
-            app.logger.debug('HMAC of the request is valid '+ event_id)
+            app.logger.debug('HMAC of the request is valid ' + event_id)
             if payload['event_data']['content'] == "Kommen Zeit notieren":
                 app.logger.debug('Event in request is clock in, create clock out task for ' + event_id)
                 create_task(event_id)
 
             end = datetime.datetime.now()
             app.logger.info('Processed request from ' + request.headers.get('X-Real-IP') + ' in ' + (
-            end - begin_time).seconds.__str__() + 's status:accepted ' + event_id)
+                end - begin_time).seconds.__str__() + 's status:accepted ' + event_id)
             return jsonify({'status': 'accepted', 'request_id': event_id}), 200
         else:
             app.logger.info('Incoming HMAC not valid')
@@ -95,14 +113,6 @@ def handle_event():
             end - begin_time).seconds.__str__() + 's status:rejected ' + event_id)
         return jsonify({'status': 'rejected'}), 400
 
-
-@app.before_first_request
-def setup_logging():
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler = RotatingFileHandler('todoist-flask.log', maxBytes=2000000, backupCount=1)
-    handler.setLevel(logging.DEBUG)
-    handler.setFormatter(formatter)
-    app.logger.addHandler(handler)
 
 if __name__ == '__main__':
     app.run(debug=False)
