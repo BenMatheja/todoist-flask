28339242c4d6a5a4357249d70845ce79f0949f8d
Ben Matheja <post@benmatheja.de>
2017-09-09 15:31:28 +0200

    added return for invalid hmac

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ app = Flask(__name__)
 
 @app.route('/todoist')
 def index():
-    return "Welcome to todoist python API"
-
+    return jsonify({'status': 'accepted',
+                    'health': 'ok'}), 200
 
 def create_task(delivery_id):
     now = datetime.datetime.now()
@@ -29,80 +29,77 @@ def create_task(delivery_id):
     acc = datetime.timedelta(hours=10, minutes=43) + now
     clockin_time = str(now.hour) + ':' + str('%02d' % now.minute)
     clockout_time = str(acc.hour) + ':' + str('%02d' % acc.minute)
-    app.logger.debug('Clock out at ' + clockout_time)
+    # app.logger.debug('Clock out at ' + clockout_time)
 
     # If devmode is False Push Event to Todoist and create new Tasks for Clocking Out
-    app.logger.info('Create Todoist Task: ' + 'Gehen (Gekommen: ' + clockin_time + ') due at ' + clockout_time)
-    if not settings.DEV_MODE:
-        app.logger.debug('Preparing Request for Todoist')
-        api = todoist.TodoistAPI(token=settings.TODOIST_API_ACCESS)
-        app.logger.debug('API connected')
-        task = api.items.add('Gehen (Gekommen: ' + clockin_time + ')',
-                             project_id='178923234', date_string=clockout_time, labels=[2147513595],
-                             priority=3)
-        # add Todoist Trace-ID here
-        api.notes.add(task['id'], 'Delivered by ' + delivery_id)
-        api.commit()
+    app.logger.info('Create Todoist Task: ' + 'Gehen (Gekommen: ' + clockin_time + ') due at ' + clockout_time + ' for ' + delivery_id)
+    #if not settings.DEV_MODE:
+    # app.logger.debug('Preparing Request for Todoist')
+    api = todoist.TodoistAPI(token=settings.TODOIST_API_ACCESS)
+    # app.logger.debug('API connected')
+    task = api.items.add('Gehen (Gekommen: ' + clockin_time + ')',
+                         project_id='178923234', date_string=clockout_time, labels=[2147513595],
+                         priority=3)
+    # add Todoist Trace-ID here
+    api.notes.add(task['id'], delivery_id)
+    api.commit()
+    app.logger.debug('Task created for request ' + delivery_id)
 
 
 @app.route('/todoist/events/v1/items', methods=['POST'])
 def handle_event():
     begin_time = datetime.datetime.now()
+    event_id = request.headers.get('X-Todoist-Delivery-ID')
     # Check if user-agent matches to todoist webhooks
     if request.headers.get('USER-AGENT') == 'Todoist-Webhooks':
-        app.logger.debug('Incoming Request from ' + request.headers.get('X-Real-IP') + ' with matching user-agent')
-
+        app.logger.debug('Incoming Request from ' + request.headers.get('X-Real-IP') + ' with matching user-agent' + event_id)
         # Maybe use a statemachine here? Need to hop to else case from this one
         if not request.json:
-            app.logger.error('Incoming Request not json')
-            abort(400)
+            app.logger.info('Incoming Request is not valid JSON ' + event_id)
+            end = datetime.datetime.now()
+            app.logger.info('Processed request from ' + request.headers.get('X-Real-IP') + ' in ' + (
+                end - begin_time).seconds.__str__() + 's status:rejected ' + event_id )
+            return jsonify({'status': 'rejected',
+                            'reason': 'malformed request'}), 400
 
         # Take payload and compute hmac
-        app.logger.debug('Reading Request Payload')
+        app.logger.debug('Reading Request Payload ' + event_id)
         payload = request.json
         request_hmac = request.headers.get('X-Todoist-Hmac-SHA256')
-        app.logger.debug('Processing Request Payload')
+        app.logger.debug('Processing Request Payload ' + event_id)
+        app.logger.debug(payload)
         calculated_hmac = base64.b64encode(
             hmac.new(settings.TODOIST_CLIENT_SECRET, msg=str(payload), digestmod=hashlib.sha256).digest())
-        # Check if request_hmac is compliant to locally computed one
-        if request_hmac == calculated_hmac or settings.DEV_MODE:
-            app.logger.debug('Hmac of the request is valid')
-            if payload['event_data']['content'] == "Kommen Zeit notieren" or settings.DEV_MODE:
-                app.logger.debug('Event is clock in, create clock out task')
-                # create task
-                create_task(request.headers.get('X-Todoist-Delivery-ID'))
+        app.logger.debug('Processed Request Payload ' + event_id)
+        app.logger.debug('start comparing ' + calculated_hmac + ' with ' + request_hmac + ' for ' + event_id)
+        if request_hmac == calculated_hmac:
+            app.logger.debug('HMAC of the request is valid '+ event_id)
+            if payload['event_data']['content'] == "Kommen Zeit notieren":
+                app.logger.debug('Event in request is clock in, create clock out task for ' + event_id)
+                create_task(event_id)
 
             end = datetime.datetime.now()
             app.logger.info('Processed request from ' + request.headers.get('X-Real-IP') + ' in ' + (
-            end - begin_time).seconds.__str__() + 's status:accepted')
-            return jsonify({'status': 'accepted', 'request_id': request.headers.get('X-Todoist-Delivery-ID')}), 200
+            end - begin_time).seconds.__str__() + 's status:accepted ' + event_id)
+            return jsonify({'status': 'accepted', 'request_id': event_id}), 200
+        else:
+            app.logger.info('Incoming HMAC not valid')
+            end = datetime.datetime.now()
+            app.logger.info('Processed request from ' + request.headers.get('X-Real-IP') + ' in ' + (
+                end - begin_time).seconds.__str__() + 's status:rejected ' + event_id)
+            return jsonify({'status': 'rejected',
+                            'reason': 'invalid request'}), 400
     else:
         end = datetime.datetime.now()
         app.logger.info('Processed request from ' + request.headers.get('X-Real-IP') + ' in ' + (
-            end - begin_time).seconds.__str__() + 's status:rejected')
+            end - begin_time).seconds.__str__() + 's status:rejected ' + event_id)
         return jsonify({'status': 'rejected'}), 400
 
-    # Initiate Loghandler
-    # https://stackoverflow.com/questions/26578733/why-is-flask-application-not-creating-any-logs-when-hosted-by-gunicorn
-    # andler = RotatingFileHandler('todoist-flask.log', maxBytes=10000, backupCount=1)
-    # gunicorn_error_logger = logging.getLogger('gunicorn.error')
-    # app.logger.handlers.extend(gunicorn_error_logger.handlers)
-    # app.logger.setLevel(logging.DEBUG)
-
-    #handler.setFormatter(formatter)
-    #app.logger.addHandler(handler)
-    # gunicorn_error_handlers = logging.getLogger('gunicorn.error').handlers
-    # app.logger.handlers.extend(gunicorn_error_handlers)
-    # app.logger.addHandler(handler)
-    # app.logger.info('my info')
-    # app.logger.debug('debug message')
 
 @app.before_first_request
 def setup_logging():
-    #if not app.debug:
-        # In production mode, add log handler to sys.stderr.
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler = RotatingFileHandler('todoist-flask.log', maxBytes=20000, backupCount=1)
+    handler = RotatingFileHandler('todoist-flask.log', maxBytes=2000000, backupCount=1)
     handler.setLevel(logging.DEBUG)
     handler.setFormatter(formatter)
     app.logger.addHandler(handler)
